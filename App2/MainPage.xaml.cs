faea05aed967151a68db9ed340a0ee99d955e932
Vicky <Vicky@VICKY-PC>
2016-09-28 23:48:22 +0800

    Add project files.

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace App2
+{
+
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
